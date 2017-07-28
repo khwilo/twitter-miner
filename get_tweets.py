f40b16b09d427761fa562a7f2f005660b51aea95
Khwilo Kabaka <kwhyzzy@gmail.com>
2017-07-28 21:04:22 +0300

    Rename file

--- get_tweets.py
+++
@@ -1,9 +0,0 @@
-from tweepy import Cursor
-from twitter_client import get_twitter_client
-
-if __name__ == '__main__':
-  client = get_twitter_client()
-  
-
-  for status in Cursor(client.home_timeline).items(10):
-    print(status.text)
